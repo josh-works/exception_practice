e0f8a96240b24bc40529e367621e0363e0ee09dc
Josh Thompson <thompsonjoshd@gmail.com>
2018-05-18 15:10:56 -0600

    fully stubbed and mocked service obj

--- lib/connection.rb
+++ lib/connection.rb
@@ -20,19 +20,3 @@ class Connection
 
   class ServiceNotFound < StandardError; end
 end
-
-
-# class Service
-#   attr_reader :status_code
-#
-#   def initialize
-#     @status_code = get_service_status
-#     # status_code would be 200, 504, 404, etc
-#   end
-#
-#   def get_service_status
-#     # do complicated stuff here to get status code
-#     # return a status code
-#     200
-#   end
-# end
--- test/connection_test.rb
+++ test/connection_test.rb
@@ -9,48 +9,22 @@ require "mocha/minitest"
 
 class ConnectionTest < Minitest::Test
 
-  def test_connect_to_external_service_returns_200
-    skip
-    conn = Connection.new
-    srv = Service.new
+  def setup
+    @conn = Connection.new
+    @srv = stub('Service object')
 
-    assert_equal 200, conn.connect_to_external_service(srv)
+  end
+
+  def test_connect_to_external_service_returns_200
+    @srv.stubs(:status_code).returns(200)
+    assert_equal 200, @conn.connect_to_external_service(@srv)
   end
 
   def test_raise_error_when_service_returns_4xx_using_stubs
-    skip
-    serv = Service.new
-    Service.any_instance.stubs(:status_code).returns(404)
-    conn = Connection.new
+    @srv.stubs(:status_code).returns(404)
 
     assert_raises Connection::ServiceNotFound do
-      conn.connect_to_external_service(serv)
-    end
-  end
-
-  # this test doesn't require the `Service` class, though all others do
-  def test_rais_error_when_service_returns_4xx_using_mocks
-    conn = Connection.new
-    srvc = stub("srvc is now a stub.")
-    # srvc.class == Mocha::Mock
-
-    srvc.stubs(:status_code).returns(404)
-
-    assert_raises Connection::ServiceNotFound do
-      conn.connect_to_external_service(srvc)
-    end
-  end
-
-  def test_raises_error_when_service_returns_4xx_no_mocks
-    skip
-    # this fails because Service#status_code is not writable
-    # from outside the object
-    srvc = Service.new
-    srvc.status_code = 404
-    conn = Connection.new
-
-    assert_raises Connection::ServiceNotFound do
-      conn.connect_to_external_service(srvc)
+      @conn.connect_to_external_service(@srv)
     end
   end
 
