dd54c0f14e3b8c5c8dff9bd1666fea8686ed718c
Josh Thompson <thompsonjoshd@gmail.com>
2018-05-18 14:43:21 -0600

    all different manners of testing

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -9,11 +9,6 @@ require "mocha/minitest"
 
 class ConnectionTest < Minitest::Test
 
-  def test_initializes_with_response_code
-    c = Connection.new
-    assert_equal 200, c.status_code
-  end
-
   def test_connect_to_external_service_returns_200
     conn = Connection.new
     srv = Service.new
@@ -21,12 +16,25 @@ class ConnectionTest < Minitest::Test
     assert_equal 200, conn.connect_to_external_service(srv)
   end
 
-  def test_raise_error_when_service_returns_4xx_using_mocks
+  def test_raise_error_when_service_returns_4xx_using_stubs
+    serv = Service.new
+    Service.any_instance.stubs(:status_code).returns(404)
+    conn = Connection.new
+
+    assert_raises Connection::ServiceNotFound do
+      conn.connect_to_external_service(serv)
+    end
+  end
+
+  # this test doesn't require the `Service` class, though all others do
+  def test_rais_error_when_service_returns_4xx_using_mocks
+    skip
+
+    conn = Connection.new
     srvc = stub("srvc is now a stub.")
     # srvc.class == Mocha::Mock
 
     srvc.stubs(:status_code).returns(404)
-    conn = Connection.new
 
     assert_raises Connection::ServiceNotFound do
       conn.connect_to_external_service(srvc)
@@ -34,6 +42,8 @@ class ConnectionTest < Minitest::Test
   end
 
   def test_raises_error_when_service_returns_4xx_no_mocks
+    # this fails because Service#status_code is not writable
+    # from outside the object
     srvc = Service.new
     srvc.status_code = 404
     conn = Connection.new
